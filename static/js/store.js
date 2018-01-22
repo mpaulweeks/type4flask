47d1ff8b23ae27db181601cee3b55bb99c0f83b5
mpaulweeks <mpaulweeks@gmail.com>
2018-01-22 12:35:14 -0500

    bust cache on getJSON

--- static/js/store.js
+++ static/js/store.js
@@ -12,15 +12,24 @@
 	module.all_cards = null;
 	module.ranking_raw = null;
 
+	function getTimestamp(){
+		// unique to the second
+	  return Math.floor((new Date()).getTime() / (1000));
+	}
+	function getJSON(url, callback){
+		const timestampUrl = `${url}?v=${getTimestamp()}`;
+		$.getJSON(timestampUrl, callback);
+	}
+
 	module.load = function(callback, stack_file){
 		callback = callback || function(){};
 
 		stack_file = stack_file || STACK_FILE;
 		var multiverse_file = MULTIVERSE_FILE;
 
-	    $.getJSON(stack_file, function(data){
+	    getJSON(stack_file, function(data){
 			module.data = data;
-			$.getJSON(multiverse_file, function(multiverse){
+			getJSON(multiverse_file, function(multiverse){
 				module.multiverse = multiverse;
 	    		return callback();
 			});
@@ -29,7 +38,7 @@
 
 	module.load_cards = function(callback, stack_file){
 		var all_cards_file = ALL_CARDS_FILE;
-	    $.getJSON(all_cards_file, function(data){
+	    getJSON(all_cards_file, function(data){
 	    	var lower_data = {};
 	    	for (var key in data){
 	    		lower_data[key.toLowerCase()] = data[key];
@@ -41,10 +50,10 @@
 
 	module.load_ranking = function(callback, stack_file){
 		var ranking_file = RANKING_FILE;
-	    $.getJSON(ranking_file, function(data){
+	    getJSON(ranking_file, function(data){
 			module.ranking_raw = data;
 			module.load(callback, stack_file);
 	    });
 	};
-	
+
 })(Module('store'));
